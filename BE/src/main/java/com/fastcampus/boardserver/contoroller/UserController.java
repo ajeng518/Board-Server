2972dc4a42f6bff557ce27e3f48d04e09651c4eb
ajeng518 <elegant518@gmail.com>
2026-01-26 22:29:22 +0900

    login, regist 기능구현

--- BE/src/main/java/com/fastcampus/boardserver/contoroller/UserController.java
+++ BE/src/main/java/com/fastcampus/boardserver/contoroller/UserController.java
@@ -1,10 +1,16 @@
 package com.fastcampus.boardserver.contoroller;
 
+import com.fastcampus.boardserver.dto.UserDTO;
+import com.fastcampus.boardserver.dto.request.UserLoginRequest;
+import com.fastcampus.boardserver.dto.response.LoginResponse;
 import com.fastcampus.boardserver.service.impl.UserServiceImpl;
+import com.fastcampus.boardserver.utils.SessionUtil;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -15,5 +21,41 @@ public class UserController {
     @Autowired
     public UserController(UserServiceImpl userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("sign-up")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signUp(@RequestBody UserDTO userDTO) {
+        if (UserDTO.hasNullDataBeforeRegister(userDTO)) {
+            throw new NullPointerException("회원가입시 필수 데이터를 모두 입력해야 합니다.");
+        }
+        userService.register(userDTO);
+    }
+
+    @PostMapping("sign-in")
+    public HttpStatus login(@RequestBody UserLoginRequest userLoginRequest,
+                            HttpSession session) {
+        ResponseEntity<LoginResponse> responseEntity = null;
+        String id = userLoginRequest.getUserId();
+        String password = userLoginRequest.getPassword();
+        LoginResponse loginResponse;
+        UserDTO userInfo = userService.login(id, password);
+
+        if (userInfo == null) {
+            return HttpStatus.NOT_FOUND;
+        } else if (userInfo != null) {
+            loginResponse = LoginResponse.success(userInfo);
+            if (userInfo.getStatus() == (UserDTO.Status.ADMIN))
+                SessionUtil.setLoginAdminId(session, id);
+            else
+                SessionUtil.setLoginMemberId(session, id);
+
+            responseEntity = new ResponseEntity<LoginResponse>(loginResponse, HttpStatus.OK);
+        } else {
+            throw new RuntimeException("Login Error! 유저 정보가 없거나 지워진 유저 정보입니다.");
+        }
+
+        return HttpStatus.OK;
+
     }
 }
