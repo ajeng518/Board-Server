ef7f720600b8344d7c9d6acc1bccba2be2ee326b
ajeng518 <elegant518@gmail.com>
2026-02-03 22:36:47 +0900

    feat: logout, password update

--- BE/src/main/java/com/fastcampus/boardserver/contoroller/UserController.java
+++ BE/src/main/java/com/fastcampus/boardserver/contoroller/UserController.java
@@ -1,8 +1,11 @@
 package com.fastcampus.boardserver.contoroller;
 
 import com.fastcampus.boardserver.dto.UserDTO;
+import com.fastcampus.boardserver.dto.request.UserDeleteId;
 import com.fastcampus.boardserver.dto.request.UserLoginRequest;
+import com.fastcampus.boardserver.dto.request.UserUpdatePasswordRequest;
 import com.fastcampus.boardserver.dto.response.LoginResponse;
+import com.fastcampus.boardserver.dto.response.UserInfoResponse;
 import com.fastcampus.boardserver.service.impl.UserServiceImpl;
 import com.fastcampus.boardserver.utils.SessionUtil;
 import jakarta.servlet.http.HttpSession;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @Log4j2
 public class UserController {
     private final UserServiceImpl userService;
+    private static final ResponseEntity<LoginResponse> FAIL_RESPONSE = new ResponseEntity<LoginResponse>(HttpStatus.BAD_REQUEST);
 
     @Autowired
     public UserController(UserServiceImpl userService) {
@@ -58,4 +62,58 @@ public class UserController {
         return HttpStatus.OK;
 
     }
+
+    @GetMapping("my-info")
+    public UserInfoResponse memberInfo(HttpSession session){
+        String id = SessionUtil.getLoginMemberId(session);
+        if(id==null)
+            id = SessionUtil.getLoginAdminId(session);
+        UserDTO memberInfo = userService.getUserInfo(id);
+
+        return new UserInfoResponse(memberInfo);
+    }
+
+    @PutMapping("logout")
+    public void logout (String accountId, HttpSession session){
+        SessionUtil.clear(session);
+    }
+
+    @PatchMapping("password")
+    public ResponseEntity<LoginResponse> updateUserPassword(String accountId, @RequestBody UserUpdatePasswordRequest userUpdatePasswordRequest, HttpSession session){
+        ResponseEntity<LoginResponse> responseEntity = null;
+        String Id = accountId;
+        String beforePassword = userUpdatePasswordRequest.getBeforePassword();
+        String afterPassword = userUpdatePasswordRequest.getAfterPassword();
+
+        try {
+            userService.updatePassword(Id, beforePassword, afterPassword);
+            UserDTO userInfo = userService.login(Id, afterPassword);
+            LoginResponse loginResponse = LoginResponse.success(userInfo);
+            ResponseEntity.ok(new ResponseEntity<LoginResponse>(loginResponse, HttpStatus.OK));
+        }catch (IllegalStateException e){
+            log.error("updatePassword 실패", e);
+            responseEntity = FAIL_RESPONSE;
+        }
+
+        return responseEntity;
+    }
+
+    @DeleteMapping
+    public ResponseEntity<LoginResponse> deleteId(@RequestBody UserDeleteId userDeleteId, HttpSession session){
+        ResponseEntity<LoginResponse> responseEntity = null;
+        UserDTO userInfo = userService.login(userDeleteId.getId(), userDeleteId.getPassword());
+        String Id = SessionUtil.getLoginMemberId(session);
+        LoginResponse loginResponse = LoginResponse.success(userInfo);
+        try{
+            userService.deleteId(Id, userDeleteId.getPassword());
+            responseEntity = new ResponseEntity<LoginResponse>(loginResponse, HttpStatus.OK);
+
+        }catch (RuntimeException e){
+            log.info(("deleteID 실패"));
+            responseEntity = FAIL_RESPONSE;
+        }
+
+        return responseEntity;
+    }
+
 }
--- BE/src/main/java/com/fastcampus/boardserver/service/impl/UserServiceImpl.java
+++ BE/src/main/java/com/fastcampus/boardserver/service/impl/UserServiceImpl.java
@@ -55,7 +55,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO getUserInfo(String userId) {
-        return null;
+        return userProfileMapper.getUserProfile(userId);
     }
 
     @Override
