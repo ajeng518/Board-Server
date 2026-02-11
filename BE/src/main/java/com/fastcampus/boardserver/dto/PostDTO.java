63924704191e23b8af4f53d9b6db48c62208a87b
ajeng518 <elegant518@gmail.com>
2026-02-11 23:15:56 +0900

    feat: post registerPost, postinfo

---
+++ BE/src/main/java/com/fastcampus/boardserver/dto/PostDTO.java
@@ -0,0 +1,23 @@
+package com.fastcampus.boardserver.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDTO {
+    private int id;
+    private String name;
+    private int isAdmin;
+    private String contents;
+    private Date createTime;
+    private int view;
+    private int userId;
+    private int fileId;
+    private Date updateTime;
+}
